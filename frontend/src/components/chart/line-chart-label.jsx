e67b8f9bf5a245eed77d393bf2186e89397edbde
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-01 23:51:42 +0400

    Fix(Report): Line chart report fix insight color

--- frontend/src/components/chart/line-chart-label.jsx
+++ frontend/src/components/chart/line-chart-label.jsx
@@ -74,12 +74,12 @@ export function ChartLineLabel({ report }) {
 					<>
 						<div className="flex gap-2 leading-none font-medium">
 							{report?.percentage_difference?.event == "Increased" ? (
-								<div className="flex items-center gap-2">
+								<div className="flex items-center gap-2 text-green-500">
 									<span>Trending up by {report?.percentage_difference?.value}% this month </span>
 									<TrendingUp className="h-4 w-4" />
 								</div>
 							) : report?.percentage_difference?.event == "Decreased" ? (
-								<div className="flex items-center gap-2">
+								<div className="flex items-center gap-2 text-red-500">
 									<span>Trending dropped by {report?.percentage_difference?.value}% this month </span>
 									<TrendingDown className="h-4 w-4" />
 								</div>
