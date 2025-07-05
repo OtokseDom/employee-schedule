dde3b19da0318e090119af1b8b99f50e212071e9
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-05 20:32:35 +0400

    Add(Dashobard): Add task status chart report and fetch

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,43 +1,46 @@
-import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axiosClient from "@/axios.client";
 import { SectionCard } from "@/components/chart/section-card";
 import { useLoadContext } from "@/contexts/LoadContextProvider";
+import { PieChartDonut } from "@/components/chart/pie-chart-donut";
 // TODO: Datatable sort not working properly. Sorting by text instead of date value
 export default function UserProfile() {
-	const { loading, setLoading } = useLoadContext();
-	const { id } = useParams(); // Get user ID from URL
+	const { setLoading } = useLoadContext();
+	const [reports, setReports] = useState();
 
-	// useEffect(() => {
-	// 	fetchData();
-	// }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-	// const fetchData = async () => {
-	// 	setLoading(true);
-	// 	try {
-	// 		// Fetch all user reports in one call
-	// 		const reportsRes = await axiosClient.get(`/user/${id}/reports`);
-	// 		setUserReports(reportsRes.data.data);
-	// 		setLoading(false);
-	// 	} catch (e) {
-	// 		console.error("Error fetching data:", e);
-	// 	} finally {
-	// 		setLoading(false);
-	// 	}
-	// };
+	const fetchData = async () => {
+		setLoading(true);
+		try {
+			// Fetch all user reports in one call
+			const reportsRes = await axiosClient.get(`/dashboard`);
+			setReports(reportsRes.data.data);
+			setLoading(false);
+		} catch (e) {
+			console.error("Error fetching data:", e);
+		} finally {
+			setLoading(false);
+		}
+	};
 
 	return (
-		<div className="flex flex-col w-screen md:w-full h-full overflow-auto bg-card text-card-foreground border border-border rounded-md container p-4 md:p-10 shadow-md">
+		<div className="flex flex-col w-screen md:w-full h-full gap-4 overflow-auto bg-card text-card-foreground border border-border rounded-md container p-4 md:p-10 shadow-md">
 			<div className="w-full mb-5">
 				<h1 className="font-extrabold text-3xl">Dashboard</h1>
 				<p>Your workspace at a glance</p>
 			</div>
 			{/* -------------------------------- first row ------------------------------- */}
-			<div className="flex flex-row gap-4">
+			<div className="flex flex-col md:flex-row gap-4">
 				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
 				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
 				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
 				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
+			</div>
+			<div className="flex flex-col md:flex-row gap-4">
+				<PieChartDonut report={reports?.tasks_by_status?.data} variant="solid" />
 			</div>
 		</div>
 	);
