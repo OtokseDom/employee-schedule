17423eba1abbea8197185c040b45d846d0a4134e
Dominic Escoto <imjohndominic08@gmail.com>
2025-07-10 21:45:30 +0400

    Add(Dashboard Report): performance leaderboard datatable

--- frontend/src/pages/Dashboard/columns.jsx
+++ frontend/src/pages/Dashboard/columns.jsx
@@ -1,38 +1,29 @@
 "use client";
-import { ArrowUpDown } from "lucide-react";
-// Define your columns without TypeScript types
+
 export const columns = [
 	{
-		accessorKey: "status",
-		// header: "Status",
-		header: ({ column }) => {
+		id: "rowNumber",
+		header: "#",
+		cell: ({ row }) => row.index + 1,
+	},
+	{
+		id: "name",
+		accessorKey: "name",
+		header: "Name",
+		cell: ({ row }) => {
+			const name = row.original.name;
+			const position = row.original.position;
 			return (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Status <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
+				<div className="min-w-52">
+					{name}
+					<br />
+					<span className="text-sm text-gray-500">{position}</span>
+				</div>
 			);
 		},
 	},
 	{
-		accessorKey: "email",
-		// header: "Email",
-		header: ({ column }) => {
-			return (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Email <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
-			);
-		},
-	},
-	{
-		accessorKey: "amount",
-		// header: "Amount",
-		header: ({ column }) => {
-			return (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Amount <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
-			);
-		},
+		accessorKey: "avg_performance_rating",
+		header: "Performance Rating",
 	},
 ];
--- frontend/src/pages/Dashboard/data-table.jsx
+++ frontend/src/pages/Dashboard/data-table.jsx
@@ -1,85 +1,78 @@
 "use client";
 
-import { useState } from "react";
-import { flexRender, getSortedRowModel, getFilteredRowModel, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
-// import { Button } from "@/components/ui/button";
-import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Input } from "@/components/ui/input";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 
-// Convert the DataTable component to JavaScript
-export function DataTable({ columns, data }) {
-	const [sorting, setSorting] = useState([]);
-	const [columnFilters, setColumnFilters] = useState([]);
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useLoadContext } from "@/contexts/LoadContextProvider";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function DataTable({ columns = [], data = [] }) {
+	const { loading } = useLoadContext();
 	const table = useReactTable({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
-		getPaginationRowModel: getPaginationRowModel(),
-		onSortingChange: setSorting,
-		getSortedRowModel: getSortedRowModel(),
-		onColumnFiltersChange: setColumnFilters,
-		getFilteredRowModel: getFilteredRowModel(),
-		state: {
-			sorting,
-			columnFilters,
-		},
 	});
 
 	return (
-		<div>
-			<div className="flex justify-end py-4">
-				<Input
-					placeholder={"filter email..."}
-					value={table.getColumn("email")?.getFilterValue() || ""}
-					onChange={(event) => table.getColumn("email")?.setFilterValue(event.target.value)}
-					className="max-w-sm"
-				/>
-			</div>
-			<div className="rounded-md">
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => {
-									return (
-										<TableHead key={header.id}>
-											{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-										</TableHead>
-									);
-								})}
+		<div className="rounded-md border">
+			<Table className="text-xs">
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+									</TableHead>
+								);
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{loading ? (
+						// Show skeleton while loading
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24">
+								<div className="flex items-center justify-center">
+									<div className="flex flex-col space-y-3 w-full">
+										{Array.from({ length: 6 }).map((_, i) => (
+											<Skeleton key={i} index={i * 0.9} className="h-24 w-full" />
+										))}
+									</div>
+								</div>
+							</TableCell>
+						</TableRow>
+					) : table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && "selected"}
+								style={
+									row.index === 0
+										? { backgroundColor: "hsl(270 70% 50% / 0.6)" }
+										: row.index === 1
+										? { backgroundColor: "hsl(270 70% 50% / 0.4)" }
+										: row.index === 2
+										? { backgroundColor: "hsl(270 70% 50% / 0.2)" }
+										: {}
+								}
+							>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+								))}
 							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody>
-						{table.getRowModel().rows?.length ? (
-							table.getRowModel().rows.map((row) => (
-								<TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-									))}
-								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell colSpan={columns.length} className="h-24 text-center">
-									No results.
-									{/* <div className="flex items-center justify-center">{true ? <Loader2 className="animate-spin" /> : "No Results."}</div> */}
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
-			<div className="flex items-center justify-end space-x-2 py-4">
-				<Button variant="outline" size="" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-					<ChevronLeft />
-				</Button>
-				<Button variant="outline" size="" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-					<ChevronRight />
-				</Button>
-			</div>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24 text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
 		</div>
 	);
 }
