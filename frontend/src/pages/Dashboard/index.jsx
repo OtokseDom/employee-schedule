c1b1bf40ec9c0e6b5d5d4443a7fd0c732c2a9164
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-11 20:06:46 +0400

    Change(Dashboard): Reposition charts

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -33,40 +33,86 @@ export default function UserProfile() {
 	};
 
 	return (
-		<div className="flex flex-col w-screen md:w-full h-fit gap-4 overflow-auto bg-card text-card-foreground border border-border rounded-md container p-4 md:p-10 shadow-md">
-			<div className="w-full mb-5">
+		<div className="w-screen md:w-fit grid grid-cols-1 md:grid-cols-12 gap-4 auto-rows-auto bg-background rounded-2xl p-4 md:p-10 border border-border">
+			<div className="md:col-span-12">
 				<h1 className="font-extrabold text-3xl">Dashboard</h1>
 				<p>Your workspace at a glance</p>
 			</div>
-			{/* -------------------------------- first row ------------------------------- */}
-			<div className="flex flex-col md:flex-row gap-4">
-				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
-				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
-				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
-				<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
+			{/* Section Cards */}
+			<div className="md:col-span-3">
+				<SectionCard description="test" value={100} percentage={12.2} insight="sample" footer="sample" />
 			</div>
-			<div className="flex flex-col md:flex-row gap-4 w-full h-fit">
+			<div className="md:col-span-3">
+				<SectionCard description="test" value={100} percentage={12.2} insight="sample" footer="sample" />
+			</div>
+			<div className="md:col-span-3">
+				<SectionCard description="test" value={100} percentage={12.2} insight="sample" footer="sample" />
+			</div>
+			<div className="md:col-span-3">
+				<SectionCard description="test" value={100} percentage={12.2} insight="sample" footer="sample" />
+			</div>
+
+			{/* Pie Chart */}
+			<div className="md:col-span-4">
 				<PieChartDonut report={reports?.tasks_by_status} variant="dashboard" />
+			</div>
+
+			{/* Multi Bar Chart */}
+			<div className="md:col-span-4">
 				<ChartBarMultiple report={reports?.estimate_vs_actual} variant="dashboard" />
 			</div>
-			<div className="flex flex-col md:flex-row gap-4 w-full h-fit">
-				{/* Chart - 3/4 width, auto height */}
-				<div className="w-full">
-					<ChartBarHorizontal report={reports?.users_task_load} variant="dashboard" />
-				</div>
 
-				{/* DataTable - 1/4 width, scrollable */}
-				<div className="w-full max-h-[600px] overflow-auto scrollbar-custom bg-primary-foreground text-card-foreground border border-border rounded-2xl container px-4 shadow-md">
-					<CardHeader>
-						<CardTitle>
-							Performance Leaderboard{" "}
-							{reports?.performance_leaderboard?.length > 0 ? "(Top " + reports?.performance_leaderboard?.length + ")" : ""}
-						</CardTitle>
-						<CardDescription>All Time</CardDescription>
-					</CardHeader>
-					<DataTable columns={columns} data={reports?.performance_leaderboard} />
-				</div>
+			{/* Horizontal Bar Chart */}
+			<div className="md:col-span-4">
+				<ChartBarHorizontal report={reports?.users_task_load} variant="dashboard" />
+			</div>
+
+			{/* Datatable */}
+			<div className="md:col-span-12 max-h-[600px] overflow-auto scrollbar-custom bg-primary-foreground text-card-foreground border border-border rounded-md container px-4 shadow-md">
+				<CardHeader>
+					<CardTitle>
+						Performance Leaderboard {reports?.performance_leaderboard?.length > 0 ? "(Top " + reports?.performance_leaderboard?.length + ")" : ""}
+					</CardTitle>
+					<CardDescription>All Time</CardDescription>
+				</CardHeader>
+				<DataTable columns={columns} data={reports?.performance_leaderboard} />
 			</div>
 		</div>
+
+		// <div className="flex flex-col w-screen md:w-full h-fit gap-4 overflow-auto bg-card text-card-foreground border border-border rounded-md container p-4 md:p-10 shadow-md">
+		// 	<div className="w-full mb-5">
+		// 		<h1 className="font-extrabold text-3xl">Dashboard</h1>
+		// 		<p>Your workspace at a glance</p>
+		// 	</div>
+		// 	{/* -------------------------------- first row ------------------------------- */}
+		// 	<div className="flex flex-col md:flex-row gap-4">
+		// 		<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
+		// 		<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
+		// 		<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
+		// 		<SectionCard description={"test"} value={100} percentage={12.2} insight={"sample"} footer={"sample"} />
+		// 	</div>
+		// 	<div className="flex flex-col md:flex-row gap-4 w-full h-fit">
+		// 		<PieChartDonut report={reports?.tasks_by_status} variant="dashboard" />
+		// 		<ChartBarMultiple report={reports?.estimate_vs_actual} variant="dashboard" />
+		// 	</div>
+		// 	<div className="flex flex-col md:flex-row gap-4 w-full h-fit">
+		// 		{/* Chart - 3/4 width, auto height */}
+		// 		<div className="w-full">
+		// 			<ChartBarHorizontal report={reports?.users_task_load} variant="dashboard" />
+		// 		</div>
+
+		// 		{/* DataTable - 1/4 width, scrollable */}
+		// 		<div className="w-full max-h-[600px] overflow-auto scrollbar-custom bg-primary-foreground text-card-foreground border border-border rounded-2xl container px-4 shadow-md">
+		// 			<CardHeader>
+		// 				<CardTitle>
+		// 					Performance Leaderboard{" "}
+		// 					{reports?.performance_leaderboard?.length > 0 ? "(Top " + reports?.performance_leaderboard?.length + ")" : ""}
+		// 				</CardTitle>
+		// 				<CardDescription>All Time</CardDescription>
+		// 			</CardHeader>
+		// 			<DataTable columns={columns} data={reports?.performance_leaderboard} />
+		// 		</div>
+		// 	</div>
+		// </div>
 	);
 }
