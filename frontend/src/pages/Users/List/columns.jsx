b987d6a3757dbd3e63b731f991af2db5465ccc84
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-14 23:45:03 +0400

    Optimize(User): Optimize code for user column

--- frontend/src/pages/Users/List/columns.jsx
+++ frontend/src/pages/Users/List/columns.jsx
@@ -5,22 +5,26 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { useAuthContext } from "@/contexts/AuthContextProvider";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useLoadContext } from "@/contexts/LoadContextProvider";
 import { useToast } from "@/contexts/ToastContextProvider";
 import axiosClient from "@/axios.client";
 
 export const columns = ({ fetchData, handleDelete, setIsOpen, setUpdateData }) => {
 	const { user } = useAuthContext();
-	const { loading, setLoading } = useLoadContext();
+	const { setLoading } = useLoadContext();
 	const showToast = useToast();
+	const navigate = useNavigate();
 	const [dialogOpen, setDialogOpen] = useState(false);
-	const [dialogType, setDialogType] = useState(null); // "reject" or "delete"
-	const [selectedUserId, setSelectedUserId] = useState(null); // pass user id here because using userRow.id in dialog gets wong id
+	const [dialogType, setDialogType] = useState(null);
+	const [selectedUserId, setSelectedUserId] = useState(null);
+	const [selectedUserData, setSelectedUserData] = useState(null);
 
-	const openDialog = (type) => {
+	const openDialog = (type, userData) => {
 		setDialogType(type);
 		setDialogOpen(true);
+		setSelectedUserId(userData.id);
+		setSelectedUserData(userData);
 	};
 
 	const handleUpdateUser = (user, event) => {
@@ -32,166 +36,116 @@ export const columns = ({ fetchData, handleDelete, setIsOpen, setUpdateData }) =
 	const handleApproval = async (action, id, userRow = {}) => {
 		setLoading(true);
 		try {
-			if (action == 0) {
-				const userResponse = await axiosClient.delete(`/user/${id}`);
-				if (userResponse.data.success == true) {
-					showToast("Success!", userResponse.data.message, 3000);
+			if (action === 0) {
+				const res = await axiosClient.delete(`/user/${id}`);
+				if (res.data.success) {
+					showToast("Success!", res.data.message, 3000);
 					navigate("/users");
 				} else {
-					showToast("Failed!", userResponse.message, 3000);
+					showToast("Failed!", res.message, 3000);
 				}
 			} else {
-				setLoading(true);
-				const form = {
-					...userRow,
-					status: "active",
-				};
-				try {
-					user;
-					const userResponse = await axiosClient.put(`/user/${id}`, form);
-					fetchData();
-					showToast("Success!", userResponse.data.message, 3000);
-				} catch (e) {
-					showToast("Failed!", e.response?.data?.message, 3000, "fail");
-					console.error("Error fetching data:", e);
-				} finally {
-					setLoading(false);
-				}
+				const form = { ...userRow, status: "active" };
+				const res = await axiosClient.put(`/user/${id}`, form);
+				fetchData();
+				showToast("Success!", res.data.message, 3000);
 			}
 		} catch (e) {
 			showToast("Failed!", e.response?.data?.message, 3000, "fail");
-			console.error("Error fetching data:", e);
 		} finally {
-			// Always stop loading when done
 			setLoading(false);
 		}
 	};
-	// TODO: Add user profile picture
-	// Define the base columns
-	const columns = [
-		{
-			id: "name",
-			accessorKey: "name",
-			header: ({ column }) => (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Name <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
-			),
-			cell: ({ row }) => {
-				const name = row.original.name;
-				const id = row.original.id;
-				const status = row.original.status;
-				return (
-					<div className="flex items-center gap-2">
-						<div className="min-w-14 min-h-14 bg-foreground rounded-full"></div>
-						<div className="flex flex-col w-full">
-							{name} {user?.id === id && <span className="text-xs text-yellow-800"> (Me)</span>}
-							<span
-								className={`backdrop-blur-sm px-2 py-1 text-xs w-fit rounded-full items-center
-                            ${
-								status == "pending"
-									? "text-yellow-100 bg-yellow-900/50"
-									: status == "active"
-									? "text-green-100 bg-green-900/50"
-									: status == "inactive"
-									? "text-gray-100 bg-gray-900/50"
-									: status == "banned"
-									? "text-red-100 bg-red-900/50"
-									: ""
-							}`}
-							>
-								{status}
-							</span>
-						</div>
-					</div>
-				);
-			},
-		},
-		{
-			id: "role",
-			accessorKey: "role",
-			header: ({ column }) => (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Role <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
-			),
-		},
-		{
-			id: "email",
-			accessorKey: "email",
-			header: ({ column }) => (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Email <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
-			),
-		},
-		{
-			id: "position",
-			accessorKey: "position",
-			header: ({ column }) => (
-				<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Position <ArrowUpDown className="ml-2 h-4 w-4" />
-				</button>
-			),
-		},
-	];
 
-	// Add Actions column only if user is Superadmin
+	const baseColumns = useMemo(
+		() => [
+			{
+				id: "name",
+				accessorKey: "name",
+				header: ({ column }) => (
+					<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+						Name <ArrowUpDown className="ml-2 h-4 w-4" />
+					</button>
+				),
+				cell: ({ row }) => {
+					const { name, id, status } = row.original;
+					return (
+						<div className="flex items-center gap-2">
+							<div className="min-w-14 min-h-14 bg-foreground rounded-full"></div>
+							<div className="flex flex-col w-full">
+								{name} {user?.id === id && <span className="text-xs text-yellow-800"> (Me)</span>}
+								<span
+									className={`backdrop-blur-sm px-2 py-1 text-xs w-fit rounded-full ${
+										status === "pending"
+											? "text-yellow-100 bg-yellow-900/50"
+											: status === "active"
+											? "text-green-100 bg-green-900/50"
+											: status === "inactive"
+											? "text-gray-100 bg-gray-900/50"
+											: status === "banned"
+											? "text-red-100 bg-red-900/50"
+											: ""
+									}`}
+								>
+									{status}
+								</span>
+							</div>
+						</div>
+					);
+				},
+			},
+			{ id: "role", accessorKey: "role", header: createHeader("Role") },
+			{ id: "email", accessorKey: "email", header: createHeader("Email") },
+			{ id: "position", accessorKey: "position", header: createHeader("Position") },
+		],
+		[user]
+	);
+
 	if (user?.data?.role === "Superadmin") {
-		columns.push({
+		baseColumns.push({
 			id: "actions",
 			cell: ({ row }) => {
 				const userRow = row.original;
 				return (
-					<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+					<>
 						<DropdownMenu modal={false}>
-							<DropdownMenuTrigger
-								asChild
-								onClick={(event) => event.stopPropagation()} // Prevent row click
-							>
+							<DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
 								<Button variant="ghost" className="h-8 w-8 p-0">
-									<span className="sr-only">Open menu</span>
 									<MoreHorizontal className="h-4 w-4" />
 								</Button>
 							</DropdownMenuTrigger>
 							<DropdownMenuContent align="end">
-								{userRow?.status == "pending" ? (
+								{userRow.status === "pending" && (
 									<>
 										<DropdownMenuItem
-											className="cursor-pointer text-green-500"
+											className="text-green-500"
 											onClick={(e) => {
 												e.stopPropagation();
-												handleApproval(1, userRow?.id, userRow);
+												handleApproval(1, userRow.id, userRow);
 											}}
 										>
 											Approve User
 										</DropdownMenuItem>
 										<DropdownMenuItem
-											className="cursor-pointer text-red-500"
+											className="text-red-500"
 											onClick={(e) => {
 												e.stopPropagation();
-												openDialog("reject");
-												setSelectedUserId(userRow.id);
+												openDialog("reject", userRow);
 											}}
 										>
 											Reject User
 										</DropdownMenuItem>
 										<hr />
 									</>
-								) : (
-									""
 								)}
-								<DropdownMenuItem className="cursor-pointer">
-									<Link to={`/profile/` + userRow.id}>View Profile</Link>
+								<DropdownMenuItem>
+									<Link to={`/profile/${userRow.id}`}>View Profile</Link>
 								</DropdownMenuItem>
-								<DropdownMenuItem className="cursor-pointer" onClick={(event) => handleUpdateUser(userRow, event)}>
-									Update User
-								</DropdownMenuItem>
+								<DropdownMenuItem onClick={(e) => handleUpdateUser(userRow, e)}>Update User</DropdownMenuItem>
 								<DropdownMenuItem
 									onClick={(e) => {
 										e.stopPropagation();
-										setSelectedUserId(userRow.id);
-										openDialog("delete");
+										openDialog("delete", userRow);
 									}}
 								>
 									<DialogTrigger asChild>
@@ -200,33 +154,44 @@ export const columns = ({ fetchData, handleDelete, setIsOpen, setUpdateData }) =
 								</DropdownMenuItem>
 							</DropdownMenuContent>
 						</DropdownMenu>
-						<DialogContent onClick={(event) => event.stopPropagation()}>
-							<DialogHeader>
-								<DialogTitle>Are you absolutely sure?</DialogTitle>
-								<DialogDescription>This action cannot be undone.</DialogDescription>
-							</DialogHeader>
-							<DialogFooter>
-								<DialogClose asChild>
-									<Button type="button" variant="secondary">
-										Close
+
+						<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+							<DialogContent onClick={(e) => e.stopPropagation()}>
+								<DialogHeader>
+									<DialogTitle>Are you absolutely sure?</DialogTitle>
+									<DialogDescription>This action cannot be undone.</DialogDescription>
+								</DialogHeader>
+								<DialogFooter>
+									<DialogClose asChild>
+										<Button type="button" variant="secondary">
+											Close
+										</Button>
+									</DialogClose>
+									<Button
+										onClick={() => {
+											if (dialogType === "reject") handleApproval(0, selectedUserId);
+											else if (dialogType === "delete") handleDelete(selectedUserId);
+										}}
+									>
+										Yes, delete
 									</Button>
-								</DialogClose>
-								<Button
-									onClick={() => {
-										if (dialogType === "reject") handleApproval(0, selectedUserId);
-										else if (dialogType === "delete") handleDelete(selectedUserId);
-										setDialogOpen(false);
-									}}
-								>
-									Yes, delete
-								</Button>
-							</DialogFooter>
-						</DialogContent>
-					</Dialog>
+								</DialogFooter>
+							</DialogContent>
+						</Dialog>
+					</>
 				);
 			},
 		});
 	}
 
-	return columns;
+	return baseColumns;
 };
+
+const createHeader =
+	(label) =>
+	({ column }) =>
+		(
+			<button className="flex" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+				{label} <ArrowUpDown className="ml-2 h-4 w-4" />
+			</button>
+		);
