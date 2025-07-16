966f015d3ced77bd43ba3c2acf765ec3dae1b9a3
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-16 20:18:34 +0400

    Fixed(Details): Dialog trigger is not inside dialog tag. Moved dialog closing tag outside.

--- frontend/src/pages/Users/Show/details.jsx
+++ frontend/src/pages/Users/Show/details.jsx
@@ -73,89 +73,89 @@ export default function UserDetails({ user, handleUpdateUser, handleApproval }) 
 						</Button>
 					</DialogFooter>
 				</DialogContent>
+
+				<div className="flex flex-col gap-3 w-full">
+					{loading ? (
+						<div className="flex gap-5">
+							<Skeleton className="w-24 h-24 rounded-full" />
+							<div className="flex flex-col gap-2">
+								<Skeleton className="w-60 h-10 rounded-full" />
+								<Skeleton className="w-60 h-10 rounded-full" />
+							</div>
+						</div>
+					) : (
+						<div className="flex items-start justify-between w-full">
+							<div className="flex gap-5 items-center">
+								<div>
+									<div className="w-24 h-24 bg-foreground rounded-full"></div>
+								</div>
+								<div className="w-full">
+									<span className="flex gap-3 text-md md:text-3xl font-bold text-white mb-0 md:mb-2">{name}</span>
+									<span className="text-xs md:text-lg text-purple-200">{position}</span>
+								</div>
+							</div>
+
+							{isEditable && (
+								<DropdownMenu modal={false}>
+									<DropdownMenuTrigger asChild>
+										<Button variant="default" className="flex items-center bg-foreground text-background">
+											<Edit size={20} />
+										</Button>
+									</DropdownMenuTrigger>
+									<DropdownMenuContent align="end">
+										{user?.status == "pending" && (
+											<>
+												<DropdownMenuItem className="cursor-pointer text-green-500" onClick={() => handleApproval(1, user.id)}>
+													Approve User
+												</DropdownMenuItem>
+												<DropdownMenuItem className="cursor-pointer text-red-500" onClick={() => openDialog("reject")}>
+													Reject User
+												</DropdownMenuItem>
+												<hr />
+											</>
+										)}
+										<DropdownMenuItem className="cursor-pointer" onClick={() => handleUpdateUser(user)}>
+											Update Account
+										</DropdownMenuItem>
+										<DropdownMenuItem onClick={() => openDialog("delete")}>
+											<DialogTrigger asChild>
+												<span className="cursor-pointer">Deactivate Account</span>
+											</DialogTrigger>
+										</DropdownMenuItem>
+									</DropdownMenuContent>
+								</DropdownMenu>
+							)}
+						</div>
+					)}
+
+					{loading ? (
+						<div className="flex gap-5">
+							<Skeleton className="w-24 h-8 rounded-full" />
+							<Skeleton className="w-24 h-8 rounded-full" />
+						</div>
+					) : (
+						<div className="flex flex-wrap mt-4 gap-2 md:gap-4">
+							<div
+								className={`backdrop-blur-sm px-3 py-1 w-fit rounded-full flex items-center ${
+									status === "pending"
+										? "text-yellow-100 bg-yellow-900/50"
+										: status === "active"
+										? "text-green-100 bg-green-900/50"
+										: status === "inactive"
+										? "text-gray-100 bg-gray-900/50"
+										: status === "banned"
+										? "text-red-100 bg-red-900/50"
+										: ""
+								}`}
+							>
+								{status}
+							</div>
+							<div className="bg-purple-900/50 backdrop-blur-sm px-3 py-1 w-fit rounded-full flex items-center text-purple-100">✨{role}</div>
+							<div className="bg-indigo-900/50 backdrop-blur-sm px-3 py-1 w-fit rounded-full flex items-center text-indigo-100">🌌 {email}</div>
+						</div>
+					)}
+				</div>
 			</Dialog>
-
-			<div className="flex flex-col gap-3 w-full">
-				{loading ? (
-					<div className="flex gap-5">
-						<Skeleton className="w-24 h-24 rounded-full" />
-						<div className="flex flex-col gap-2">
-							<Skeleton className="w-60 h-10 rounded-full" />
-							<Skeleton className="w-60 h-10 rounded-full" />
-						</div>
-					</div>
-				) : (
-					<div className="flex items-start justify-between w-full">
-						<div className="flex gap-5 items-center">
-							<div>
-								<div className="w-24 h-24 bg-foreground rounded-full"></div>
-							</div>
-							<div className="w-full">
-								<span className="flex gap-3 text-md md:text-3xl font-bold text-white mb-0 md:mb-2">{name}</span>
-								<span className="text-xs md:text-lg text-purple-200">{position}</span>
-							</div>
-						</div>
-
-						{isEditable && (
-							<DropdownMenu modal={false}>
-								<DropdownMenuTrigger asChild>
-									<Button variant="default" className="flex items-center bg-foreground text-background">
-										<Edit size={20} />
-									</Button>
-								</DropdownMenuTrigger>
-								<DropdownMenuContent align="end">
-									{user?.status == "pending" && (
-										<>
-											<DropdownMenuItem className="cursor-pointer text-green-500" onClick={() => handleApproval(1, user.id)}>
-												Approve User
-											</DropdownMenuItem>
-											<DropdownMenuItem className="cursor-pointer text-red-500" onClick={() => openDialog("reject")}>
-												Reject User
-											</DropdownMenuItem>
-											<hr />
-										</>
-									)}
-									<DropdownMenuItem className="cursor-pointer" onClick={() => handleUpdateUser(user)}>
-										Update Account
-									</DropdownMenuItem>
-									<DropdownMenuItem onClick={() => openDialog("delete")}>
-										<DialogTrigger asChild>
-											<span className="cursor-pointer">Deactivate Account</span>
-										</DialogTrigger>
-									</DropdownMenuItem>
-								</DropdownMenuContent>
-							</DropdownMenu>
-						)}
-					</div>
-				)}
-
-				{loading ? (
-					<div className="flex gap-5">
-						<Skeleton className="w-24 h-8 rounded-full" />
-						<Skeleton className="w-24 h-8 rounded-full" />
-					</div>
-				) : (
-					<div className="flex flex-wrap mt-4 gap-2 md:gap-4">
-						<div
-							className={`backdrop-blur-sm px-3 py-1 w-fit rounded-full flex items-center ${
-								status === "pending"
-									? "text-yellow-100 bg-yellow-900/50"
-									: status === "active"
-									? "text-green-100 bg-green-900/50"
-									: status === "inactive"
-									? "text-gray-100 bg-gray-900/50"
-									: status === "banned"
-									? "text-red-100 bg-red-900/50"
-									: ""
-							}`}
-						>
-							{status}
-						</div>
-						<div className="bg-purple-900/50 backdrop-blur-sm px-3 py-1 w-fit rounded-full flex items-center text-purple-100">✨{role}</div>
-						<div className="bg-indigo-900/50 backdrop-blur-sm px-3 py-1 w-fit rounded-full flex items-center text-indigo-100">🌌 {email}</div>
-					</div>
-				)}
-			</div>
 		</div>
 	);
 }
