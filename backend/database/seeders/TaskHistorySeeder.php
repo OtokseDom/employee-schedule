aee5faf55d8775c0a74242a2b9e5c3d51e856a32
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-26 21:33:20 +0400

    Refactor(Task History Seeder): Add created remark to all seeded tasks

--- backend/database/seeders/TaskHistorySeeder.php
+++ backend/database/seeders/TaskHistorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 use App\Models\TaskHistory;
+use App\Models\User;
 
 class TaskHistorySeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class TaskHistorySeeder extends Seeder
      */
     public function run(): void
     {
-        TaskHistory::factory()->count(10)->create();
+        // TaskHistory::factory()->count(10)->create();
+
+        $userId = User::inRandomOrder()->value('id');
+
+        Task::all()->each(function ($task) use ($userId) {
+            TaskHistory::create([
+                'organization_id' => $task->organization_id,
+                'task_id' => $task->id,
+                'status' => 'Pending',
+                'changed_by' => $userId,
+                'changed_at' => now(),
+                'remarks' => 'Task Added',
+            ]);
+        });
     }
 }
