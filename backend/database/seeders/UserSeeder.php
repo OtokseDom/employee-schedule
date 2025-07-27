7f785128c202570c6aa4a3a3ea1c72198237c3f5
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-07-27 22:51:16 +0400

    Change(User seeder): Add all role account

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
@@ -15,17 +16,72 @@ class UserSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'organization_id' => 1,
-            'name' => "Admin User",
-            'dob' => Carbon::parse('2000-11-08'),
-            'position' => "Software Engineer",
-            'role' => "Superadmin",
-            'email' => "admin@demo.com", //dom@gmail.com
-            'email_verified_at' => now(),
-            'password' => 'admin123', // 1
-            'status' => 'active', // active, inactive, pending, banned
-            'remember_token' => Str::random(10),
-        ]);
+
+        $users = [
+            [
+                'organization_id' => 1,
+                'name' => "Superadmin User",
+                'dob' => Carbon::parse('2000-11-08'),
+                'position' => "Software Engineer",
+                'role' => "Superadmin",
+                'email' => "superadmin@demo.com", //dom@gmail.com
+                'email_verified_at' => now(),
+                'password' => 'admin123', // 1
+                'status' => 'active', // active, inactive, pending, banned
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'organization_id' => 1,
+                'name' => "Admin User",
+                'dob' => Carbon::parse('2000-11-08'),
+                'position' => "Software Engineer",
+                'role' => "Superadmin",
+                'email' => "admin@demo.com", //dom@gmail.com
+                'email_verified_at' => now(),
+                'password' => 'admin123', // 1
+                'status' => 'active', // active, inactive, pending, banned
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'organization_id' => 1,
+                'name' => "Manager User",
+                'dob' => Carbon::parse('2000-11-08'),
+                'position' => "Software Engineer",
+                'role' => "Manager",
+                'email' => "manager@demo.com", //dom@gmail.com
+                'email_verified_at' => now(),
+                'password' => 'admin123', // 1
+                'status' => 'active', // active, inactive, pending, banned
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'organization_id' => 1,
+                'name' => "Employee User",
+                'dob' => Carbon::parse('2000-11-08'),
+                'position' => "Software Engineer",
+                'role' => "Employee",
+                'email' => "employee@demo.com", //dom@gmail.com
+                'email_verified_at' => now(),
+                'password' => 'admin123', // 1
+                'status' => 'active', // active, inactive, pending, banned
+                'remember_token' => Str::random(10),
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+        // \App\Models\User::factory()->create([
+        //     'organization_id' => 1,
+        //     'name' => "Admin User",
+        //     'dob' => Carbon::parse('2000-11-08'),
+        //     'position' => "Software Engineer",
+        //     'role' => "Superadmin",
+        //     'email' => "admin@demo.com", //dom@gmail.com
+        //     'email_verified_at' => now(),
+        //     'password' => 'admin123', // 1
+        //     'status' => 'active', // active, inactive, pending, banned
+        //     'remember_token' => Str::random(10),
+        // ]);
     }
 }
