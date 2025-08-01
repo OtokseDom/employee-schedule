7aa46e1736af9147984496d7b2e44606c50392a7
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-08-01 13:35:18 +0400

    Add(Backend API Versioning): added v1 prefix for all apis

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,31 +13,33 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
 
-    // Route::get('/user-auth', function (Request $request) {
-    //     return $request->user();
-    // });
-    // Eager load organization
-    Route::get('/user-auth', function (Request $request) {
-        $user = User::with('organization')->find($request->user()->id);
-        return new UserResource($user);
+        // Route::get('/user-auth', function (Request $request) {
+        //     return $request->user();
+        // });
+        // Eager load organization
+        Route::get('/user-auth', function (Request $request) {
+            $user = User::with('organization')->find($request->user()->id);
+            return new UserResource($user);
+        });
+        Route::post('/logout', [AuthController::class, 'logout']);
+        /* --------------------------------- Masters -------------------------------- */
+        // User CRUD
+        Route::apiResource('/organization', OrganizationController::class);
+        Route::apiResource('/user', UserController::class);
+        Route::apiResource('/category', CategoryController::class);
+        Route::apiResource('/task', TaskController::class);
+        Route::apiResource('/task-history', TaskHistoryController::class);
+
+        /* --------------------------------- Reports -------------------------------- */
+        Route::get('/user/{id}/reports', [UserReportController::class, 'userReports']);
+        Route::get('/dashboard', [DashboardReportController::class, 'dashboardReports']);
+
+        /* ---------------------------------- OTHER --------------------------------- */
+        Route::patch('/organization/{organization}/generate-code', [OrganizationController::class, 'generateCode']);
     });
-    Route::post('/logout', [AuthController::class, 'logout']);
-    /* --------------------------------- Masters -------------------------------- */
-    // User CRUD
-    Route::apiResource('/organization', OrganizationController::class);
-    Route::apiResource('/user', UserController::class);
-    Route::apiResource('/category', CategoryController::class);
-    Route::apiResource('/task', TaskController::class);
-    Route::apiResource('/task-history', TaskHistoryController::class);
-
-    /* --------------------------------- Reports -------------------------------- */
-    Route::get('/user/{id}/reports', [UserReportController::class, 'userReports']);
-    Route::get('/dashboard', [DashboardReportController::class, 'dashboardReports']);
-
-    /* ---------------------------------- OTHER --------------------------------- */
-    Route::patch('/organization/{organization}/generate-code', [OrganizationController::class, 'generateCode']);
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
