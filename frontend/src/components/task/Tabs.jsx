dd563dbb57945683218a39f56ba866e9f5a2faee
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-08-14 23:21:09 +0400

    Add(Tassk Tabs): created tabs component to reuse

---
+++ frontend/src/components/task/Tabs.jsx
@@ -0,0 +1,38 @@
+import { Loader2 } from "lucide-react";
+
+export default function Tabs({ loading, updateData, activeTab, setActiveTab, parentId }) {
+	return (
+		<div className="flex flex-row gap-5">
+			{Object.keys(updateData).length > 0 ? (
+				<div className="flex flex-row items-center">
+					<div className="flex flex-row w-fit h-fit border-b-2 border-b-bg-secondary bg-card text-sm">
+						<div className={`w-fit py-2 px-5 ${activeTab == "update" ? "bg-secondary" : "text-muted-foreground"} rounded-t`}>
+							<button onClick={() => setActiveTab("update")}>Update Task</button>
+						</div>
+						<div className={`w-fit py-2 px-5 ${activeTab == "history" ? "bg-secondary" : "text-muted-foreground"} rounded-t`}>
+							<button onClick={() => setActiveTab("history")}>History</button>
+						</div>
+						<div className={`w-fit py-2 px-5 ${activeTab == "relations" ? "bg-secondary" : "text-muted-foreground"} rounded-t`}>
+							<button onClick={() => setActiveTab("relations")}>Relations</button>
+						</div>
+					</div>
+					<span>{loading && <Loader2 className="animate-spin" />}</span>
+				</div>
+			) : parentId ? (
+				<div className="flex flex-row items-center">
+					<div className="flex flex-row w-fit h-fit border-b-2 border-b-bg-secondary bg-card text-sm">
+						<div className={`w-fit py-2 px-5 ${activeTab == "update" ? "bg-secondary" : "text-muted-foreground"} rounded-t`}>
+							<button onClick={() => setActiveTab("update")}>Add Subtask</button>
+						</div>
+						<div className={`w-fit py-2 px-5 ${activeTab == "relations" ? "bg-secondary" : "text-muted-foreground"} rounded-t`}>
+							<button onClick={() => setActiveTab("relations")}>Relations</button>
+						</div>
+					</div>
+					<span>{loading && <Loader2 className="animate-spin" />}</span>
+				</div>
+			) : (
+				"Add Task"
+			)}
+		</div>
+	);
+}
