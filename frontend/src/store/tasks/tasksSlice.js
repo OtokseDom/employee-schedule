eb00b32f455a76aa36160bc4a469b255ab86400e
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-08-16 22:51:18 +0400

    Update(TaskSlice): added history and relations

--- frontend/src/store/tasks/tasksSlice.js
+++ frontend/src/store/tasks/tasksSlice.js
@@ -17,4 +17,46 @@ export const createTasksSlice = (set) => ({
 		set((state) => ({
 			tasks: state.tasks.filter((t) => t.id !== id),
 		})),
+
+	// Task History
+	taskHistories: [],
+	selectedTaskHistory: {},
+
+	setTaskHistory: (taskHistory) => set({ taskHistory }),
+	setSelectedTaskHistory: (selectedTaskHistory) => set({ selectedTaskHistory }),
+
+	addTaskHistory: (taskHistory) =>
+		set((state) => ({
+			taskHistories: [...state.taskHistories, taskHistory],
+		})),
+
+	updateTaskHistory: (id, updates) =>
+		set((state) => ({
+			taskHistories: state.taskHistories.map((t) => (t.id === id ? { ...t, ...updates } : t)),
+		})),
+
+	removeTaskHistory: (id) =>
+		set((state) => ({
+			taskHistories: state.taskHistories.filter((t) => t.id !== id),
+		})),
+
+	// Relations
+	relations: [],
+
+	setRelations: (relations) => set({ relations }),
+
+	addRelation: (relation) =>
+		set((state) => ({
+			relations: [...state.relations, relation],
+		})),
+
+	updateRelation: (id, updates) =>
+		set((state) => ({
+			relations: state.relations.map((t) => (t.id === id ? { ...t, ...updates } : t)),
+		})),
+
+	removeRelation: (id) =>
+		set((state) => ({
+			relations: state.relations.filter((t) => t.id !== id),
+		})),
 });
--- frontend/src/store/tasks/tasksStore.js
+++ frontend/src/store/tasks/tasksStore.js
@@ -7,5 +7,19 @@ export const useTasksStore = () => {
 		addTask: useAppStore((state) => state.addTask),
 		updateTask: useAppStore((state) => state.updateTask),
 		removeTask: useAppStore((state) => state.removeTask),
+		// Task History
+		taskHistories: useAppStore((state) => state.taskHistories),
+		selectedTaskHistory: useAppStore((state) => state.selectedTaskHistory),
+		setTaskHistory: useAppStore((state) => state.setTaskHistory),
+		setSelectedTaskHistory: useAppStore((state) => state.setSelectedTaskHistory),
+		addTaskHistory: useAppStore((state) => state.addTaskHistory),
+		updateTaskHistory: useAppStore((state) => state.updateTaskHistory),
+		removeTaskHistory: useAppStore((state) => state.removeTaskHistory),
+		// Relations
+		relations: useAppStore((state) => state.relations),
+		setRelations: useAppStore((state) => state.setRelations),
+		addRelation: useAppStore((state) => state.addRelation),
+		updateRelation: useAppStore((state) => state.updateRelation),
+		removeRelation: useAppStore((state) => state.removeRelation),
 	};
 };
