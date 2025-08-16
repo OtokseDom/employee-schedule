bfcac121cbe872056ec15d64e5ae4c447c9fcde2
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-08-16 22:52:33 +0400

    Add(appStore): new stores

--- frontend/src/store/appStore.js
+++ frontend/src/store/appStore.js
@@ -3,10 +3,14 @@ import { createTasksSlice } from "./tasks/tasksSlice";
 import { createUsersSlice } from "./users/usersSlice";
 import { createDashboardSlice } from "./dashboard/dashboardSlice";
 import { createProjectsSlice } from "./projects/projectsSlice";
+import { createUserSlice } from "./user/userSlice";
+import { createCategoriesSlice } from "./categories/categoriesSlice";
 
 export const useAppStore = create((set) => ({
 	...createDashboardSlice(set),
 	...createProjectsSlice(set),
 	...createTasksSlice(set),
 	...createUsersSlice(set),
+	...createUserSlice(set),
+	...createCategoriesSlice(set),
 }));
