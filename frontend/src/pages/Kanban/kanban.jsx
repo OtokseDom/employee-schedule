165ee44a449b0b6a8ca601248918c27890ea3e74
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-09-07 12:21:57 +0400

    Update(kanban): Puth handler functions in handleDragEnd for smooth dragging experience

--- frontend/src/pages/Kanban/kanban.jsx
+++ frontend/src/pages/Kanban/kanban.jsx
@@ -126,23 +126,69 @@ export default function KanbanBoard() {
 			newItems[overContainerIndex].items.push(removedItem);
 			setContainers(newItems);
 		}
+	};
+	// Wrap with debounce (10ms delay)
+	const debouncedHandleDragMove = debounce(handleDragMove, 10);
 
-		// Originally in handleDragEnd
-		// Hanlde container sorting
-		if (active.id.toString().includes("container") && over.id.toString().includes("container") && active && over && active.id !== over.id) {
+	const handleDragEnd = ({ active, over }) => {
+		// Handling Container Sorting
+		if (active.id.toString().includes("container") && over?.id.toString().includes("container") && active && over && active.id !== over.id) {
+			// Find the index of the active and over container
 			const activeContainerIndex = containers.findIndex((container) => container.id === active.id);
 			const overContainerIndex = containers.findIndex((container) => container.id === over.id);
-
 			// Swap the active and over container
 			let newItems = [...containers];
 			newItems = arrayMove(newItems, activeContainerIndex, overContainerIndex);
 			setContainers(newItems);
 		}
-	};
-	// Wrap with debounce (10ms delay)
-	const debouncedHandleDragMove = debounce(handleDragMove, 10);
 
-	const handleDragEnd = () => {
+		// Handling item Sorting
+		if (active.id.toString().includes("item") && over?.id.toString().includes("item") && active && over && active.id !== over.id) {
+			// Find the active and over container
+			const activeContainer = findValueOfItems(active.id, "item");
+			const overContainer = findValueOfItems(over.id, "item");
+
+			// If the active or over container is not found, return
+			if (!activeContainer || !overContainer) return;
+			// Find the index of the active and over container
+			const activeContainerIndex = containers.findIndex((container) => container.id === activeContainer.id);
+			const overContainerIndex = containers.findIndex((container) => container.id === overContainer.id);
+			// Find the index of the active and over item
+			const activeitemIndex = activeContainer.items.findIndex((item) => item.id === active.id);
+			const overitemIndex = overContainer.items.findIndex((item) => item.id === over.id);
+
+			// In the same container
+			if (activeContainerIndex === overContainerIndex) {
+				let newItems = [...containers];
+				newItems[activeContainerIndex].items = arrayMove(newItems[activeContainerIndex].items, activeitemIndex, overitemIndex);
+				setContainers(newItems);
+			} else {
+				// In different containers
+				let newItems = [...containers];
+				const [removeditem] = newItems[activeContainerIndex].items.splice(activeitemIndex, 1);
+				newItems[overContainerIndex].items.splice(overitemIndex, 0, removeditem);
+				setContainers(newItems);
+			}
+		}
+		// Handling item dropping into Container
+		if (active.id.toString().includes("item") && over?.id.toString().includes("container") && active && over && active.id !== over.id) {
+			// Find the active and over container
+			const activeContainer = findValueOfItems(active.id, "item");
+			const overContainer = findValueOfItems(over.id, "container");
+
+			// If the active or over container is not found, return
+			if (!activeContainer || !overContainer) return;
+			// Find the index of the active and over container
+			const activeContainerIndex = containers.findIndex((container) => container.id === activeContainer.id);
+			const overContainerIndex = containers.findIndex((container) => container.id === overContainer.id);
+			// Find the index of the active and over item
+			const activeitemIndex = activeContainer.items.findIndex((item) => item.id === active.id);
+
+			let newItems = [...containers];
+			const [removeditem] = newItems[activeContainerIndex].items.splice(activeitemIndex, 1);
+			newItems[overContainerIndex].items.push(removeditem);
+			setContainers(newItems);
+		}
 		setActiveId(null);
 	};
 
@@ -167,6 +213,9 @@ export default function KanbanBoard() {
 							}}
 						>
 							<SortableContext items={container.items.map((i) => i.id)}>
+								{/* {container.items.length === 0 && (
+									<div className="py-[15vh] text-sm text-muted-foreground border rounded-md">Drop items here</div>
+								)} */}
 								<div className="flex items-start flex-col gap-y-4">
 									{container.items.map((item) => (
 										<Items key={item.id} id={item.id} title={item.title} description={item.description} />
