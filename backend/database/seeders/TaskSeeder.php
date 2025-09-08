2d0a3e618d96d9de66bdd340a04dabc43f346537
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-09-08 22:06:01 +0400

    Update(task factory): adjusted factory because of added unique constraint in task migration

--- backend/database/seeders/TaskSeeder.php
+++ backend/database/seeders/TaskSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Project;
 use Illuminate\Database\Seeder;
 use App\Models\Task;
-use App\Models\TaskStatus;
 use Illuminate\Support\Facades\DB;
 
 class TaskSeeder extends Seeder
@@ -19,25 +17,6 @@ class TaskSeeder extends Seeder
         DB::table('tasks')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        // Step 1: Seed tasks
-        Task::factory()->count(50)->create(); // or more tasks
-
-        // Step 2: Update positions per project and status
-        $projects = Project::all();
-        $statuses = TaskStatus::all();
-
-        foreach ($projects as $project) {
-            foreach ($statuses as $status) {
-                $tasks = Task::where('project_id', $project->id)
-                    ->where('status_id', $status->id)
-                    ->orderBy('created_at')
-                    ->get();
-
-                $position = 1;
-                foreach ($tasks as $task) {
-                    $task->update(['position' => $position++]);
-                }
-            }
-        }
+        Task::factory()->count(50)->create();
     }
 }
