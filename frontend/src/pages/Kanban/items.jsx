96206ead504211c263bb8663f0d80fd22975ad0d
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-09-10 21:40:05 +0400

    Update(kanban): Move mark check of items on top

--- frontend/src/pages/Kanban/items.jsx
+++ frontend/src/pages/Kanban/items.jsx
@@ -5,7 +5,7 @@ import clsx from "clsx";
 import { Check, Text } from "lucide-react";
 import { Toggle } from "@/components/ui/toggle";
 
-const Items = ({ id, title, description }) => {
+const Items = ({ id, title, description, position }) => {
 	const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
 		id: id,
 		data: {
@@ -21,16 +21,24 @@ const Items = ({ id, title, description }) => {
 				transform: CSS.Translate.toString(transform),
 			}}
 			className={clsx(
-				"p-2 bg-sidebar-accent shadow-md rounded-md w-full border border-transparent hover:border-foreground cursor-pointer",
+				"p-2 bg-sidebar-accent shadow-md rounded-md w-full border border-transparent hover:border-foreground cursor-pointer group",
 				isDragging && "opacity-50"
 			)}
 		>
-			<div className="w-full p-2 hover:cursor-grab active:cursor-grabbing" {...listeners} />
-			<div className="flex px-1 items-center justify-start gap-3 group">
-				<Toggle variant="outline" size={"xxs"} aria-label="Toggle" className="rounded-full self-start hidden group-hover:block data-[state=on]:block">
+			<div className="flex flex-row w-full">
+				<Toggle
+					variant="outline"
+					size={"xxs"}
+					aria-label="Toggle"
+					className="rounded-full self-start opacity-0 group-hover:opacity-100 data-[state=on]:opacity-100 transition-all duration-200 ease-in-out"
+				>
 					<Check />
 				</Toggle>
+				<div className="w-full py-3 hover:cursor-grab active:cursor-grabbing" {...listeners} />
+			</div>
+			<div className="flex px-1 items-center justify-start gap-3">
 				<div className="w-full">
+					<p>{position}</p>
 					<p>{title}</p>
 					<p>{description && <Text size={14} className="text-muted-foreground" />}</p>
 				</div>
