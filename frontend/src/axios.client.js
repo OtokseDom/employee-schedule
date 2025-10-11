9f30bae270541b13b518a8ac51e23180eaf12fdf
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-10-11 20:33:32 +0400

    axios to remove /api prefix on url

--- frontend/src/axios.client.js
+++ frontend/src/axios.client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
 	// where port is running via .env
-	baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+	baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
 });
 
 // interceptors executed before request is sent or after response is received
