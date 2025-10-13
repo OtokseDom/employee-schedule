a530ac8d03210464c9b5b28b76e9507c3829e56a
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2025-10-13 21:17:34 +0400

    Update(Dashboard card): rating to show (5)

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -126,7 +126,7 @@ export default function UserProfile() {
 			{/* Section Cards */}
 			<div className="flex flex-col md:flex-row gap-4 md:col-span-12 overflow-auto">
 				{/* <SectionCard description={`Active Members`} showBadge={false} value={reports?.section_cards?.user_count} variant="dashboard" /> */}
-				<SectionCard description="Members Avg Performance (10)" showBadge={false} value={reports?.section_cards?.avg_performance} variant="dashboard" />
+				<SectionCard description="Members Avg Performance (5)" showBadge={false} value={reports?.section_cards?.avg_performance} variant="dashboard" />
 				<SectionCard description="Avg Time Efficiency" showBadge={false} value={`${reports?.section_cards?.time_efficiency}%`} variant="dashboard" />
 				<SectionCard description="Avg Completion Rate" showBadge={false} value={`${reports?.section_cards?.completion_rate}%`} variant="dashboard" />
 				{/* <SectionCard description="Tasks Due Soon" showBadge={false} value={reports?.section_cards?.task_at_risk} variant="dashboard" /> */}
